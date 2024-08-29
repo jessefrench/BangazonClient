07cc0c40952fdbaddfb8f7808933e648e72d0dd9
Jesse <frejes32@gmail.com>
2024-08-28 22:04:09 -0500

    Escaped user auth hell

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,41 @@
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import { useEffect, useState } from 'react';
+import { checkUser, signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import RegisterForm from '../components/RegisterForm';
 
-function Home() {
+export default function Home() {
   const { user } = useAuth();
+  const [authUser, setAuthUser] = useState();
+
+  useEffect(() => {
+    checkUser(user.uid).then((data) => setAuthUser(data));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onUpdate = () => {
+    checkUser(user.uid).then((data) => setAuthUser(data));
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      {authUser?.uid === user?.uid ? (
+        <div
+          className="d-flex flex-column align-items-center justify-content-center"
+          style={{
+            height: '90vh',
+            padding: '30px',
+            maxWidth: '600px',
+            margin: '0 auto',
+          }}
+        >
+          <h1>Hello {user?.fbUser?.displayName}! </h1>
+          <p>Click the button below to logout!</p>
+          <Button variant="danger" type="button" size="lg" className="user-card-button" onClick={signOut}>
+            Sign Out
+          </Button>
+        </div>
+      ) : (<RegisterForm user={user} onUpdate={onUpdate} />)}
+    </>
   );
 }
-
-export default Home;
